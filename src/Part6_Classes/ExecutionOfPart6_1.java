408333d3e4faab28f9508d111fb26cc44b2f41dc
RatInTheCave <arslan.saadat@auk.edu.ua>
2023-04-15 18:48:09 +0300

    Additional Commit

--- src/Part6_Classes/ExecutionOfPart6_1.java
+++ src/Part6_Classes/ExecutionOfPart6_1.java
@@ -1,22 +1,9 @@
 package Part6_Classes;
 
-public class ExecutionOfCustomClass {
+public class ExecutionOfPart6_1 {
     public static void main(String[] args) {
-//        MyCustomClass MyObj = new MyCustomClass();
-//        MyCustomClass MyOb1j = new MyCustomClass();
-//        MyOb1j.setGrant("Frant");
-//        MyCustomClass MyOb2j = new MyCustomClass();
-//        MyCustomClass MyOb3j = new MyCustomClass();
-//        MyCustomClass MyOb4j = new MyCustomClass();
-//        MyCustomClass ReferenceToObj1 = MyOb1j;
-//        System.out.println(MyOb1j);
-//        System.out.println(MyOb1j.getGrant());
-//        System.out.println(ReferenceToObj1);
-//        System.out.println(ReferenceToObj1.getGrant());
-
         ClassesPart1 Part6 = new ClassesPart1();
         Part6.incrementCount();
-        //System.out.println(Part6);
         System.out.println(Part6.getInstanceCount());
         ClassesPart1 Object1 = new ClassesPart1();
         ClassesPart1 Object2 = new ClassesPart1();
@@ -24,7 +11,9 @@ public class ExecutionOfCustomClass {
         ClassesPart1 Object4 = new ClassesPart1();
         ClassesPart1 Object5 = new ClassesPart1();
         ClassesPart1 Object6 = new ClassesPart1();
-
+        System.out.println( "Total object count: "+ Part6.getInstanceCount());
     }
+
+
 
 }
--- src/Part6_Classes/Person.java
+++ src/Part6_Classes/Person.java
@@ -1,4 +1,53 @@
 package Part6_Classes;
 
-public class ConstOverlord {
+import java.time.Year;
+
+public class Person {
+    private String name;
+    private String surname;
+    private int birthYear;
+    public final int COMING_OF_AGE = 18;
+    public Person(){
+        setName("Not set");
+        setSurName("Not set");
+        setBirthYear(0);
+    }
+    //
+    public Person(String newName, String newSurName, int newBirthYear){
+        setName(newName);
+        setSurName(newSurName);
+        setBirthYear(newBirthYear);
+    }
+
+    //getters
+    public String getName(){
+        return name;
+    }
+    public String getSurName(){
+        return surname;
+    }
+    public int getBirthYear(){
+        return birthYear;
+    }
+    //setters
+    public void setName(String newName){
+        name = newName;
+    }
+    public void setSurName(String newSurName){
+        surname = newSurName;
+    }
+    public void setBirthYear(int newBirthYear){
+        birthYear = newBirthYear;
+    }
+
+    public void printPersonInfo(){
+        System.out.printf("This Person's name is %s, surname %s, birth year is %d, \n Adult = %b \n", name, surname
+                , birthYear, isAdult());
+    }
+
+    public boolean isAdult(){
+        int CurrentYear = Year.now().getValue();
+        boolean result = (CurrentYear - birthYear)>=COMING_OF_AGE;
+        return result;
+    }
 }
